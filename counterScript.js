79ee6b03fea1d5559995db78c001496f02072906
Adaeze <adaezeikemefuna@yahoo.com>
2021-05-29 03:57:22 +0100

    Added the file to the repo

---
+++ counterScript.js
@@ -0,0 +1,23 @@
+const increase = document.getElementById("increment-btn");
+const saveButton = document.getElementById("save-btn");
+const displayCount = document.getElementById("count-el");
+const saved = document.getElementById("saveEl");
+let count = 0;
+
+increase.addEventListener("click", increment)
+saveButton.addEventListener("click", saveCount)
+
+function increment() {
+    count = count + 1;
+    displayCount.textContent = count;
+}
+
+function saveCount() {
+    let countStr = count + " - "
+    saved.textContent += countStr;
+    displayCount.textContent = 0;
+    count = 0;
+    
+}
+
+let firstCard = 
